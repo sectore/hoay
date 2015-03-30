b3770d7d15d7a762a9b2ec1b92de900e180303c0
sectore <info@websector.de>
2015-03-30 14:58:05 +0200

    moving responsibility of navigation into mixin

--- src/main-controller.ts
+++ src/main-controller.ts
@@ -2,13 +2,7 @@
 module hoay {
   'use strict';
 
-  interface MainControllerInterface {
-    gotoInfo():void;
-    gotoSettings():void;
-    gotoResult():void;
-  }
-
-  class MainController implements MainControllerInterface {
+  class MainController {
 
     // @ngInject
     constructor(private $scope:IMainScope) {
@@ -16,19 +10,6 @@ module hoay {
       // 'vm' stands for 'view model'. An additional benefit to this is to prevent primatives getting
       // assigned to the scope directly
       $scope.vm = this;
-    }
-
-
-    gotoInfo():void {
-      console.log('goto info');
-    }
-
-    gotoSettings():void {
-      console.log('goto settings');
-    }
-
-    gotoResult():void {
-      console.log('goto result');
     }
   }
 
